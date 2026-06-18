19b4c17702517027e9b3d893edf2c98789f36afa
Ponthius <ponthiuspaul@gmail.com>
2026-06-18 10:56:46 +0300

    feat: update dashboard links to tasks and add splash screen with logo

--- server.js
+++ server.js
@@ -18,13 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const path = require('path');
-
-// Splash screen on /
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.get('/health', (req, res) => {
     res.json({ success: true, status: 'online' });
 });
